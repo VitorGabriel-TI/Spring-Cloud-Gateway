efa1c20a5b1a1a9124d90f0ac01d4e8e21b59191
Vitor Gabriel <contato.vitorgabrielti@gmail.com>
2024-01-18 05:50:07 -0300

    Initial commit

---
+++ src/test/java/br/com/vitorgabrielti/scghello/ScgHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vitorgabrielti.scghello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScgHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
